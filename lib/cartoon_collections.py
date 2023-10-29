272043d9aff8cc0fadd745c49d4017784f6efd77
Kat Tannehill <katetannehill@gmail.com>
2023-10-29 12:01:02 -0600

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for index in range(len(list)):
+        print(f"{index + 1}. {list[index]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [(element[0].upper() + element[1:] + "!") for element in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for call in list:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for element in list:
+        if element in cheeses:
+            return element
+    return None
